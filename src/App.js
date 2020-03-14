668a0c94ca338a7e9bca85a7ef875719ef615c7e
szabi.keresztes <szabi.keresztes@bjss.com>
2020-03-14 14:16:20 +0000

    Adding GA and tracking when user open landing page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 
+import ReactGA from 'react-ga';
+
 function App() {
+
+  ReactGA.event({
+    category: 'landing page',
+    action: 'User has loaded first page',
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hi!<br />I'm Szabi</h1>
-        <p>Software dev, beginner musician, health enthusiast</p>
+        <p>Software dev, beginner musician, health enthusiast</p>        
       </header>
+      <p>let's see if GA is working</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import ReactGA from 'react-ga';
+
+const trackingId = 'UA-160669098-1';
+ReactGA.initialize(trackingId);
+
+ReactGA.set({
+  user: 'a user'
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
