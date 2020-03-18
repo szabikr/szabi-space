8c2bad9c5cd46ab33a36295d277c936471fa4302
szabi.keresztes <szabi.keresztes@bjss.com>
2020-03-18 22:59:07 +0000

    Adding links to media platforms and styling changes

--- src/App.js
+++ src/App.js
@@ -12,11 +12,24 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <h1>Hi!<br />I'm Szabi</h1>
-        <p>Software dev, beginner musician, health enthusiast</p>        
+        <h2>Software developer, beginner musician, health enthusiast</h2>
       </header>
-      <p>let's see if GA is working</p>
+
+      <footer>
+        <div>
+          <code>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/szabikr/">Instagram</a> |&nbsp;
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/szabikr">Github</a> |&nbsp;
+            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/szabikr">Twitter</a> |&nbsp;
+            <a target="_blank" rel="noopener noreferrer" href="https://soundcloud.com/user-285007878/tracks">Soundcloud</a> |&nbsp;
+            <a target="_blank" rel="noopener noreferrer" href="https://dev.to/szabikr">dev.to</a> |&nbsp;
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/szabolcs-keresztes-598935ba">LinkedIn</a>
+          </code>
+        </div>
+      </footer>
+
     </div>
   );
 }
