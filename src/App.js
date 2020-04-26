c0a5dc858c12ad72db952c730e9fdd789c7aa87c
szabi.keresztes <szabi.keresztes@bjss.com>
2020-04-26 12:04:38 +0100

    All footer elements are visible without scrolling

--- src/App.js
+++ src/App.js
@@ -20,17 +20,37 @@ function App() {
 
       <footer>
         <div>
+          <img id="footer-logo" src={ logoBlueTrans } alt="Logo" />
+        </div>
+        <div>
           <code>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/szabikr/">Instagram</a> |&nbsp;
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/szabikr">Github</a> |&nbsp;
-            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/szabikr">Twitter</a> |&nbsp;
-            <a target="_blank" rel="noopener noreferrer" href="https://soundcloud.com/user-285007878/tracks">Soundcloud</a> |&nbsp;
-            <a target="_blank" rel="noopener noreferrer" href="https://dev.to/szabikr">dev.to</a> |&nbsp;
-            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/szabolcs-keresztes-598935ba">LinkedIn</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/szabikr/">Instagram</a>
           </code>
         </div>
         <div>
-          <img src={ logoBlueTrans } alt="Logo" />
+          <code>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/szabikr">Github</a>
+          </code>
+        </div>
+        <div>
+          <code>
+            <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/szabikr">Twitter</a>
+          </code>
+        </div>
+        <div>
+          <code>
+            <a target="_blank" rel="noopener noreferrer" href="https://soundcloud.com/user-285007878/tracks">Soundcloud</a>
+          </code>
+        </div>
+        <div>
+          <code>
+            <a target="_blank" rel="noopener noreferrer" href="https://dev.to/szabikr">dev.to</a>
+            </code>
+        </div>
+        <div>
+          <code>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/szabolcs-keresztes-598935ba">LinkedIn</a>
+          </code>
         </div>
       </footer>
 
