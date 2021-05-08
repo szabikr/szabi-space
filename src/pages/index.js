3e2cb844a244c8e9fa4df63e8c0c16807d15e927
szabikr <szabikr.dev@gmail.com>
2021-05-08 16:52:29 +0100

    Adding more content

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,13 @@ function App() {
     <>
       <h1>Welcome to <strong>szabi.space</strong></h1>
       <h2>Building responsive, dynamic applications powered by the cloud</h2>
+      <p>Currently experimenting with:</p>
+      <ul>
+        <li>NextJS - SSG & SSR</li>
+        <li>Jest with Testing Library</li>
+        <li>AWS S3, Lambda, API Gateway</li>
+        <li>CI/CD with GitHub Actions</li>
+      </ul>
     </>
   )
 }
