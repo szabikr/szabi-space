3cbef2c48195550d1a422738252d1d206c6f2b52
Szabi Keresztes <szabikr.dev@gmail.com>
2021-06-24 22:36:53 +0100

    Styling, Logo, Content and Weather Component (#17)
    * Displaying weather from 3rd party api
    
    * styling via App & display logo image
    
    * content and styling changes
    
    * content changes
    
    * content changes

---
+++ src/components/logo.js
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <img
+      src="/logo-transparent.png"
+      alt="szabi.space logo"
+      width="280"
+    />
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,59 @@
-function App() {
+import Weather from '../components/weather'
+import Logo from '../components/logo'
+
+export default function App() {
   return (
     <>
-      <h1>Welcome to <strong>szabi.space</strong></h1>
-      <h2>Building responsive, dynamic applications powered by the cloud</h2>
-      <p>Currently experimenting with:</p>
-      <ul>
-        <li>NextJS - SSG & SSR</li>
-        <li>Jest with Testing Library</li>
-        <li>AWS S3, Lambda, API Gateway</li>
-        <li>CI/CD with GitHub Actions</li>
-        <li>CSS3</li>
-      </ul>
-      <p>
-        Check out my CV
-        {' '}
-        <a 
-          href="/Szabi_Keresztes_Software_Engineer_CV.pdf"
-          target="_blank"
-        >
-          here
-        </a>
-      </p>
+      <header>
+        <h1>Welcome to <strong>szabi.space</strong></h1>
+        <h2>Building responsive, dynamic applications powered by the cloud.</h2>
+      </header>
+      <div className="content">
+        <Logo />
+
+        <h3>Experienced with</h3>
+        <ul>
+          <li>Front-end State Management</li>
+          <li>UI/UX Development</li>
+          <li>Agile Delivery</li>
+        </ul>
+
+        <h3>Currently experimenting with</h3>
+        <ul>
+          <li>NextJS - SSG & SSR</li>
+          <li>Jest with Testing Library</li>
+          <li>AWS Route 53, CloudFront, S3</li>
+          <li>CI/CD with GitHub Actions</li>
+          <li>CSS3</li>
+        </ul>
+        <h3>
+          Check out my CV
+          {' '}
+          <a 
+            href="/Szabi_Keresztes_Software_Engineer_CV.pdf"
+            target="_blank"
+          >
+            here
+          </a>.
+        </h3>
+      </div>
+      
+      <footer>
+        <p>
+          <code>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/szabolcs-keresztes-598935ba">LinkedIn</a>
+          </code>
+        </p>
+        <p>
+          <code>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/szabikr">Github</a>
+          </code>
+        </p>
+        <div style={{ paddingTop: 24}}>
+          <h3>Weather report for you</h3>
+          <Weather />
+        </div>
+      </footer>
     </>
   )
 }
-
-export default App;
