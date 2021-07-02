4e3df2f94d09a3a2592356e7ad370ddfef4da16e
szabikr <szabikr.dev@gmail.com>
2021-07-02 08:37:58 +0100

    Adding content to Agile page

--- src/pages/agile.js
+++ src/pages/agile.js
@@ -11,6 +11,49 @@ export default function AgilePage() {
         <li><strong>Responding to change</strong> over following a plan</li>
       </ul>
       <p>That is, while there is value in the items on the right, we value the items on the left more.</p>
+
+      <hr />
+
+      <h3>Product lifecycle</h3>
+      <p>"No surprises end-game"</p>
+      <ul>
+        <li>Feasability</li>
+        <li>Discovery</li>
+        <li>Delivery (alfa, beta, full)</li>
+        <li>Retire</li>
+      </ul>
+
+      <h3>Multi-disciplinary, self organising teams</h3>
+      <p>Bring yourself to work</p>
+      <ul>
+        <li>"T" shaped individuals</li>
+        <li>Forming, Storming, Norming and Performing</li>
+        <li>Members (main: DM, PO, SA)</li>
+      </ul>
+
+      <h3>Scrum Framework</h3>
+      <p>
+        <a href="https://www.scrum.org/resources/what-is-scrum">More details</a>
+      </p>
+      <ul>
+        <li>User stories</li>
+        <li>Board</li>
+        <li>Ceremonies</li>
+        <li>Plan -> Build -> Reflect -> Plan ...</li>
+      </ul>
+
+      <img
+        src="/assets/scrum-lifecycle.png"
+        alt="scrum lifecycle"
+        width="280"
+      />
+      <p><i>source: https://www.scrum.org/</i></p>
+
+      <img
+        src="/assets/too-busy-to-improve.png"
+        alt="too busy to improve illustration"
+        width="280"
+      />
     </>
   )
 }
