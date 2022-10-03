1f982229590ccc8dcf5bc73aab6543331ea248d1
szabikr <szabikr.dev@gmail.com>
2022-10-03 12:33:52 +0300

    Updating index page

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const sandboxRepos = [
+  {
+    name: 'Python',
+    language: 'Python',
+    repoLink: 'https://github.com/szabikr/sandbox-python',
+    iconLink: 'Python Icon',
+  },
+  {
+    name: 'Bash',
+    language: 'Unix Shell',
+    repoLink: 'https://github.com/szabikr/sandbox-bash',
+    iconLink: 'Bash Icon',
+  },
+  {
+    name: 'Postgres',
+    language: 'SQL',
+    repoLink: 'https://github.com/szabikr/sandbox-postgresql',
+    iconLink: 'Postgres Icon',
+  },
+  {
+    name: 'GH Actions',
+    language: 'YAML',
+    repoLink: 'https://github.com/szabikr/sandbox-github-actions',
+    iconLink: 'GH Actions Icon',
+  },
+  {
+    name: 'WebdriverIO',
+    language: 'JavaScript',
+    repoLink: 'https://github.com/szabikr/sandbox-webdriverio',
+    iconLink: 'WebdriverIO Icon',
+  },
+  {
+    name: 'React',
+    language: 'Javascript',
+    repoLink: 'https://github.com/szabikr/sandbox-react',
+    iconLink:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png',
+  },
+  {
+    name: 'Node',
+    language: 'Javascript',
+    repoLink: 'https://github.com/szabikr/sandbox-node',
+    iconLink: 'Node Icon',
+  },
+  {
+    name: 'Docker',
+    language: 'YAML',
+    repoLink: 'https://github.com/szabikr/sandbox-docker',
+    iconLink: 'Django Icon',
+  },
+  {
+    name: 'Web Design',
+    language: 'HTML, CSS',
+    repoLink: 'https://github.com/szabikr/sandbox-css',
+    iconLink:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png',
+  },
+]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,55 @@
-import Link from 'next/link'
-import Weather from '../components/weather'
-import Logo from '../components/logo'
+import Work from '../components/work'
 
 export default function App() {
   return (
     <>
       <header>
-        <h1>Welcome to <strong>szabi.space</strong></h1>
-        <h2>Building responsive, dynamic applications powered by the cloud.</h2>
+        <h1>Hi. I'm Szabi. A Software Engineer.</h1>
       </header>
-      <div className="content">
-        <Logo />
+      <main>
+        <section>
+          <ul>
+            <li>
+              <h4>
+                <a
+                  href="/assets/Szabi_Keresztes_Software_Engineer_CV.pdf"
+                  target="_blank"
+                >
+                  CV
+                </a>
+              </h4>
+              <p>PDF</p>
+            </li>
+            <li>
+              <h4>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/szabikr"
+                >
+                  Github
+                </a>
+              </h4>
+              <p>Profile</p>
+            </li>
+            <li>
+              <h4>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.linkedin.com/in/szabolcs-keresztes-598935ba"
+                >
+                  LinkedIn
+                </a>
+              </h4>
+              <p>Profile</p>
+            </li>
+          </ul>
+        </section>
+        <Work />
+      </main>
 
-        <h3>Experienced with</h3>
-        <ul>
-          <li>Front-end State Management</li>
-          <li>UI/UX Development</li>
-          <li><Link href="/agile"><a>Agile Delivery</a></Link></li>
-        </ul>
-
-        <h3>Currently experimenting with</h3>
-        <ul>
-          <li>NextJS - SSG & SSR</li>
-          <li>Jest with Testing Library</li>
-          <li>AWS Route 53, CloudFront, S3</li>
-          <li>CI/CD with GitHub Actions</li>
-          <li>CSS3</li>
-        </ul>
-        <h3>
-          Check out my CV
-          {' '}
-          <a 
-            href="/assets/Szabi_Keresztes_Software_Engineer_CV.pdf"
-            target="_blank"
-          >
-            here
-          </a>.
-        </h3>
-      </div>
-      
-      <footer>
-        <p>
-          <code>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/szabolcs-keresztes-598935ba">LinkedIn</a>
-          </code>
-        </p>
-        <p>
-          <code>
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/szabikr">Github</a>
-          </code>
-        </p>
-        <div style={{ paddingTop: 24}}>
-          <h3>Weather report for you</h3>
-          <Weather />
-        </div>
-      </footer>
+      <footer></footer>
     </>
   )
 }
