5d474e49a706fc0273e47f9d3c2c803fec5bc463
szabikr <szabikr.dev@gmail.com>
2023-03-07 19:02:49 +0200

    Extracts primary button styling and adds primary class to back button

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
 import '../styles/prism-one-light.css'
 import '../styles/typography.css'
+import '../styles/button.css'
 import '../styles/main.css'
 import '../styles/projects.css'
 import '../styles/inline-code.css'
