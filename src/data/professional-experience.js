970421d2e1fe30b762f379685fae78247b2c8350
szabikr <szabikr.dev@gmail.com>
2023-03-13 11:01:12 +0200

    Places different data in different files

---
+++ src/data/professional-experience.js
@@ -0,0 +1,51 @@
+export const professionalExperience = [
+  {
+    id: '94f8a13c-23d0-4193-9eec-e6dd2177955a',
+    jobTitle: 'Mentor',
+    organization: 'School of Code',
+    startDate: '2021-11-01',
+    endDate: '2023-01-01',
+  },
+  {
+    id: 'f6e739ba-82ea-4bfd-8fc8-9f0cb069c311',
+    jobTitle: 'Software Engineer',
+    organization: 'BJSS',
+    startDate: '2018-11-01',
+    endDate: '2021-12-01',
+  },
+  {
+    id: '4a7717e2-9876-42d7-ac22-53fe7198cd6e',
+    jobTitle: 'Full-Stack Developer',
+    organization: 'RightIndem',
+    startDate: '2017-02-01',
+    endDate: '2018-09-01',
+  },
+  {
+    id: 'd472caeb-dd47-4f11-9ca3-023e48fa80cd',
+    jobTitle: 'Co-Founder',
+    organization: 'Wraptime',
+    startDate: '2016-09-01',
+    endDate: '2017-02-01',
+  },
+  {
+    id: '1bfb08db-a345-4bb4-9b11-33897d27d4a4',
+    jobTitle: '.Net Developer',
+    organization: 'Branded3',
+    startDate: '2016-04-01',
+    endDate: '2016-08-01',
+  },
+  {
+    id: '36e6cfe6-f5e0-456a-b343-4dd176e29342',
+    jobTitle: '.Net Developer',
+    organization: 'ComKnow',
+    startDate: '2015-08-01',
+    endDate: '2016-02-01',
+  },
+  {
+    id: '0f4bfdc6-4a1b-4113-b3b1-e47ea99269de',
+    jobTitle: 'iOS Apprentice',
+    organization: 'Reea',
+    startDate: '2014-03-01',
+    endDate: '2014-03-01',
+  },
+]
---
+++ src/data/projects.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    name: 'Habit Tracker',
+    summary:
+      'A CLI tool that I use to track my habits. It is based on a well defined file parser.',
+    url: 'blog/refactoring-user-input-importer-in-my-habit-tracker-cli',
+    repoLink: 'https://github.com/szabikr/habit-tracker',
+  },
+  {
+    name: 'Szabi.space',
+    summary:
+      'My personal website that showcases my work, hosts my blog and describes my past software engineering experience.',
+    url: 'https://szabi.space/',
+    repoLink: 'https://github.com/szabikr/szabi-space',
+  },
+  {
+    name: 'Authentication in Single Page Application',
+    summary:
+      'A proof of concept for implementing authentication using the MERN tech stack.',
+    url: '/proof-of-concepts/authentication',
+    repoLink: 'https://github.com/szabikr/poc-authentication',
+  },
+  {
+    name: 'React Boilerplate Using Webpack',
+    summary:
+      'A project from scratch that builds React applications with lates JS features and CSS support.',
+    url: 'https://react-boilerplate-szabikr.vercel.app/',
+    repoLink: 'https://github.com/szabikr/sandbox-react/tree/main/boilerplate',
+  },
+  {
+    name: 'Quote Analyser',
+    summary:
+      'A funny little app that uses public APIs to take a quote apart and defined the words that are present.',
+    url: 'https://quote-analyser-szabikr.vercel.app/',
+    repoLink:
+      'https://github.com/szabikr/sandbox-react/tree/main/quote-analyser',
+  },
+  {
+    name: 'Todo List',
+    summary:
+      'Basic todo list application that persists your list so that you can make sure to complete every todo item.',
+    url: 'https://todo-list-szabikr.vercel.app/',
+    repoLink: 'https://github.com/szabikr/sandbox-react/tree/main/todo-list',
+  },
+]
