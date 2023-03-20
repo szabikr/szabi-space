fab68159d3dc95706763e3e85717cb54dd5e0750
szabikr <szabikr.dev@gmail.com>
2023-03-20 13:48:36 +0200

    Extends Experties section

--- src/components/experties/index.jsx
+++ src/components/experties/index.jsx
@@ -1,12 +1,33 @@
 export default function Experties() {
   return (
-    <>
-      <h4>My Experties</h4>
-      <ul>
-        <li>Software Engineering</li>
-        <li>Front-End Development</li>
-        <li>Agile Ways of Working</li>
+    <section>
+      <h3>My Experties</h3>
+      <ul className="projects">
+        <li>
+          <h5>Software Engineering</h5>
+          <p>
+            Experienced in Procedural, Functional and Object Oriented
+            Programming: TypeScript, JavaScript, Python. Excited about code
+            architecture and refactoring.
+          </p>
+        </li>
+        <li>
+          <h5>Front-End Development</h5>
+          <p>
+            Passionate about UI/UX and Application State Management. Skilled in
+            Semantic HTML, React, NextJS.
+          </p>
+        </li>
+        <li>
+          <h5>Agile Ways of Working</h5>
+          <p>
+            Was fortunate enough to gain extremely valuable experience working
+            in Scrum, Nexus and Kanban frameworks. Love working with teams that
+            are fast moving and have to deliver on time, meet the requirements
+            and ensure quality.
+          </p>
+        </li>
       </ul>
-    </>
+    </section>
   )
 }
