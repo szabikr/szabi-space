6292fa3eda36414c86ab452a370f20a1475cb043
szabikr <szabikr.dev@gmail.com>
2023-04-05 17:04:39 +0300

    Adds link to graduation thesis abstract in student section

--- src/components/experience/content/student-sapientia.tsx
+++ src/components/experience/content/student-sapientia.tsx
@@ -5,6 +5,14 @@ export default function StudentSapientia() {
         Bachelor's Degree with first-class honours in{' '}
         <strong>Computer Science</strong>.
       </p>
+      <p>
+        <a
+          target="_blank"
+          href="https://szabi.space/assets/Parallel_Implementation_of_Cellular_Automatons_-_Abstract.pdf"
+        >
+          Graduation Thesis Abstract
+        </a>
+      </p>
     </>
   )
 }
