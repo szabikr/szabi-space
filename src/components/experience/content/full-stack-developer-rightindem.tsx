d466616e996fcb938c6693b25e2db8c6db62cac4
szabikr <szabikr.dev@gmail.com>
2023-04-10 11:33:16 +0300

    Uploads content for full stack dev at ri and adds line-height styles

--- src/components/experience/content/full-stack-developer-rightindem.tsx
+++ src/components/experience/content/full-stack-developer-rightindem.tsx
@@ -1,8 +1,48 @@
 export default function FullStackDeveloperRightindem() {
   return (
     <>
-      <p>Full-Stack Developer at RightIndem</p>
-      <p>Not yet written...</p>
+      <p>
+        After realising that my previous adventure will not succeed and looking
+        at my back account to see a one digit figure only, I quickly started to
+        look for opportunities around Nottingham City Centre. RightIndem was a
+        perfect fit for me at the time because I could still stay in the
+        start-up space but also utilise my freshly acquired front-end skills.
+      </p>
+      <p>
+        At the organisation, I was responsible for building a One Question Per
+        Page front-end solution in <strong>React v15</strong> and work with our
+        bespoke, in-house <strong>CQRS</strong> system. Furthermore, I took part
+        in discussions about the technical details of our system, product
+        roadmap and design decisions. And occasionally, organising darts
+        competitions, pool tournaments and staying super late to finish a
+        feature for the next day release.
+      </p>
+      <p>A few takeaways from this job:</p>
+      <ul>
+        <li>
+          It is almost always better to pick an{' '}
+          <strong>off-the-shelves framework</strong> for doing the heavy lifting
+          on the technical side of your project because other people already
+          have had similar problems to you and they developed a solution that is
+          well thought out and tested. Focusing most of your efforts on solving
+          domain specific problems that are unique to your business is a much
+          better use of your time.
+        </li>
+        <li>
+          <strong>TDD</strong> works best when building features with well
+          defined requirements and most of the underlying technical details are
+          fleshed out and solved. It can be a very polarising topic so it's good
+          to enter in discussions about it with an open mind. The{' '}
+          <strong>Red Green Refactor</strong> approach can be very satisfying.
+        </li>
+        <li>
+          <strong>Agile teams</strong> are all different and while the Scrum or
+          any other framework specification is a good place to start you don't
+          want to stick with something that doesn't work for you. Iteratively
+          improving the ways of working, listening to your mates and having fun
+          is the core of any high-achieving, high-trust and fast moving teams.
+        </li>
+      </ul>
     </>
   )
 }
