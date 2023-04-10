cce8e1163e1b4c54250573dcb4df5d63501aa01a
szabikr <szabikr.dev@gmail.com>
2023-04-10 11:31:30 +0300

    Uploads content for mentor at school of code

--- src/components/experience/content/mentor-school-of-code.tsx
+++ src/components/experience/content/mentor-school-of-code.tsx
@@ -1,8 +1,39 @@
 export default function MentorSchoolOfCode() {
   return (
     <>
-      <p>Mentor @ School of Code</p>
-      <p>Not yet written...</p>
+      <p>
+        Throughout my career I have had a few official/unofficial mentors who
+        guided me on my software engineering path and I felt that it was my turn
+        to give back to the community.
+      </p>
+      <p>
+        School of Code is a 16 week intensive tech bootcamp that focuses on{' '}
+        <strong>web developer skills</strong>, <strong>design</strong>, and{' '}
+        <strong>agile ways of woking</strong>. My responsibility as a Mentor was
+        to guide the bootcampers on their journey not only from a technical
+        perspective but also from a professional one.
+      </p>
+      <p>
+        I helped three people from different backgrounds to understand the
+        fundamentals of <strong>JavaScript</strong>, <strong>JSX</strong>, the
+        mechanics of <strong>React</strong> such as props propagation,
+        conditional rendering, state management and common hooks. On the backend
+        we looked at <strong>RESTful APIs</strong> in the popular Express.js
+        framework and some basic SQL queries. Finally, I supported them to
+        deploy their solutions on Netlify and Heroku.
+      </p>
+      <p>
+        Along side the technical challenges we had discussions about the
+        different types of tech organisations out there and finding out what
+        would be the best first step in their careers. I provided my
+        professional experiences as an example to describe what it means to work
+        in the software engineering industry and what are the roles within an
+        Agile team.
+      </p>
+      <p>
+        This side job was a great experience and I am looking forward to
+        mentoring junior to mid level developers in the future.
+      </p>
     </>
   )
 }
