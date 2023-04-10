a56fc959fd656955031bb2f4e6e0398f38990ad4
szabikr <szabikr.dev@gmail.com>
2023-04-10 12:28:28 +0300

    Adds resume button to header

---
+++ src/components/common/resume-button.tsx
@@ -0,0 +1,11 @@
+export default function ResumeButton() {
+  return (
+    <a
+      className="button"
+      target="_blank"
+      href="https://szabi.space/assets/Szabi_Keresztes_Software_Engineer_CV.pdf"
+    >
+      Resume
+    </a>
+  )
+}
--- src/pages/lab/component-library.tsx
+++ src/pages/lab/component-library.tsx
@@ -1,35 +1,44 @@
 import BackToHomeButton from '../../components/common/back-to-home-button'
+import ResumeButton from '../../components/common/resume-button'
 
 export default function ComponentLibrary() {
   return (
     <>
       <main>
-        <h1>This is an h1 text</h1>
-        <h2>This is an h2 text</h2>
-        <h3>This is an h3 text</h3>
-        <h4>This is an h4 text</h4>
-        <h5>This is an h5 text</h5>
-        <h6>This is an h6 text</h6>
-
-        <p>
-          <strong>This is a paragraph:</strong> Lorem ipsum dolor sit amet,
-          consectetur adipiscing elit. Cras placerat ex nunc. Etiam vel aliquet
-          libero. Phasellus vitae ex sit amet nisl vehicula finibus. Nam vel
-          magna lorem. Pellentesque elementum mauris ut erat vehicula, at
-          malesuada sapien congue. In molestie dolor a erat ornare tincidunt.{' '}
-          <em>Emphasized text in paragraph.</em>{' '}
-          <strong>Strong text in paragraph.</strong> Quisque at facilisis
-          turpis. Sed ac congue diam. Nam et ex porta, bibendum felis quis,
-          fermentum dolor. Fusce ut metus sed nibh gravida dictum commodo sit
-          amet enim.
-        </p>
-
         <div>
-          <strong>bold text</strong>
+          <h1>Typography</h1>
+          <h1>This is an h1 text</h1>
+          <h2>This is an h2 text</h2>
+          <h3>This is an h3 text</h3>
+          <h4>This is an h4 text</h4>
+          <h5>This is an h5 text</h5>
+          <h6>This is an h6 text</h6>
+
+          <p>
+            <strong>This is a paragraph:</strong> Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Cras placerat ex nunc. Etiam vel
+            aliquet libero. Phasellus vitae ex sit amet nisl vehicula finibus.
+            Nam vel magna lorem. Pellentesque elementum mauris ut erat vehicula,
+            at malesuada sapien congue. In molestie dolor a erat ornare
+            tincidunt. <em>Emphasized text in paragraph.</em>{' '}
+            <strong>Strong text in paragraph.</strong> Quisque at facilisis
+            turpis. Sed ac congue diam. Nam et ex porta, bibendum felis quis,
+            fermentum dolor. Fusce ut metus sed nibh gravida dictum commodo sit
+            amet enim.
+          </p>
+
+          <div>
+            <strong>bold text</strong>
+          </div>
+
+          <div>
+            <em>emphasized text</em>
+          </div>
         </div>
 
         <div>
-          <em>emphasized text</em>
+          <h1>Resume</h1>
+          <ResumeButton />
         </div>
 
         <div>
@@ -72,7 +81,10 @@ export default function ComponentLibrary() {
           </div>
         </div>
 
-        <BackToHomeButton />
+        <div>
+          <h1>Return to Home Button</h1>
+          <BackToHomeButton />
+        </div>
       </main>
     </>
   )
