f1abbfc6b9075c500a367bd07bf2e61d148ee1c3
Szabi Keresztes <szabikr.dev@gmail.com>
2023-04-10 11:37:39 +0300

    Content in Experience Section (#69)
    * Uploads content for mentor at school of code
    
    * Uploads content for software engineer at bjss and sets it to be open by default
    
    * Uploads content for full stack dev at ri and adds line-height styles
    
    * Updates technical tag UI colour

--- src/components/experience/content/software-engineer-bjss.tsx
+++ src/components/experience/content/software-engineer-bjss.tsx
@@ -1,8 +1,80 @@
 export default function SoftwareEngineerBjss() {
   return (
     <>
-      <p>Software Engineer @ BJSS</p>
-      <p>Not yet written...</p>
+      <p>
+        Up until this point I was working mainly with start-ups and smaller
+        companies and I wanted to learn how a well established consultancy does
+        things in the engineering and project delivery space.
+      </p>
+      <p>
+        My first project at the company was a{' '}
+        <strong>digital transformation</strong>, Driving Examiner Services, that
+        currently allows driving examiners across the UK to conduct driving
+        tests on an iPad saving hundreds of thousands of paper each year. The
+        application makes driving examiners' lives easier by digitising the exam
+        process and the exam itself smoother for the candidates, reducing weeks
+        of delay between the tests and results.
+      </p>
+      <p>
+        Short video about the project:{' '}
+        <a
+          href="https://www.youtube.com/watch?v=7N_I6nvWwIg&ab_channel=BJSSLtd"
+          target="_blank"
+        >
+          DVSA Case Study: Transforming an 85 year old paper driving test
+        </a>
+        .
+      </p>
+      <p>
+        The client's requirement was for us to use the{' '}
+        <strong>Ionic framework</strong> for building the application, and our
+        solutions architect chose a serverless, microservices architecture in
+        the <strong>AWS</strong> cloud. At the time Ionic was only supporting{' '}
+        <strong>Angular</strong> so we bit the bullet and got the most out of
+        the framework that we could. The application had to deal with a complex
+        state, handling 12 different categories and working in situations where
+        the device did not have internet connection, so we picked{' '}
+        <strong>NgRx</strong> as the central nervous system of our solution.{' '}
+        <strong>TypeScript</strong> was used to build a robust schema oriented
+        codebase.
+      </p>
+      <p>
+        I was working very closely with the Product Design team, first to
+        understand their User Research and make sure that we are building the
+        right thing. And second, to implement our component library that
+        followed the <strong>Government Digital Service</strong> guidelines.
+      </p>
+      <p>
+        To deliver the product MVP, we worked in a scaled <strong>Scrum</strong>{' '}
+        environment that we called <strong>Nexus</strong>, held a demonstration
+        event every two weeks to keep our client in the loop and gather valuable
+        feedback. As a team, we built our repositories{' '}
+        <strong>open-source</strong>; reviewed and merged multiple{' '}
+        <strong>Pull Requests</strong> each day.
+      </p>
+      <p>
+        After 18 months of hard work, the client's in-house software engineering
+        team took over the development, inviting me to join them for 6 more
+        months and complete the <strong>project handover</strong>.
+      </p>
+      <p>
+        Hands down this project was my favourite and most successful work in the
+        first 5 years of my career. And I consider myself lucky for having the
+        opportunity to work alongside extremely talented people.
+      </p>
+      <p>
+        For my final year at the company I tried to switch things up so I moved
+        to Birmingham and participated in a few small to mid scale projects,
+        developing solutions on the front-end. One project in particular had a
+        very well designed <strong>CI/CD pipeline</strong> so I had the chance
+        to experience the benefits of such a process.
+      </p>
+      <p>
+        After 3 years into the consultancy business I decided to take a brake,
+        focus more on my health and fitness, reconnect with old friends & family
+        and build a few projects for myself (including this website), so I
+        handed in my notice.
+      </p>
     </>
   )
 }
