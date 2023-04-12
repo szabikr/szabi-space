5733427b92b895b899ec0ee680a38f266e0d226f
szabikr <szabikr.dev@gmail.com>
2023-04-12 21:27:34 +0300

    Tries out purple colour variantions

--- src/components/tagline/index.tsx
+++ src/components/tagline/index.tsx
@@ -12,9 +12,29 @@ export default function Tagline() {
           and testable back-end solutions that excel in code quality.
         </h2>
         <p>
-          <a href="#contact-me" className="button primary">
-            Contact Me
-          </a>
+          <div>
+            <a href="#contact-me" className="button primary">
+              Contact Me
+            </a>
+          </div>
+          <div>
+            <a
+              href="#contact-me"
+              className="button primary"
+              style={{ backgroundColor: '#8d029b' }}
+            >
+              Contact Me
+            </a>
+          </div>
+          <div>
+            <a
+              href="#contact-me"
+              className="button primary"
+              style={{ backgroundColor: '#6c3082' }}
+            >
+              Contact Me
+            </a>
+          </div>
         </p>
       </div>
       <p>
