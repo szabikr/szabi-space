d79b0b5637f1772c6ac3a46b68a8af00ba8f01a6
szabikr <szabikr.dev@gmail.com>
2023-04-16 11:38:28 +0300

    Refactor header component and styling

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,30 +7,26 @@ export default function Header() {
       <Link href="/">
         <span className="logo">szabikr.</span>
       </Link>
-      <div className="menu-items">
-        <nav>
-          <ul>
-            <li>
-              <a href="#expertise">Expertise</a>
-            </li>
-            <li>
-              <a href="#experience">Experience</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#blog">Blog</a>
-            </li>
-            <li>
-              <a href="#contact-me">Contact</a>
-            </li>
-          </ul>
-        </nav>
-        <div>
-          <ResumeButton />
-        </div>
-      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#expertise">Expertise</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+          <li>
+            <a href="#contact-me">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <ResumeButton />
     </header>
   )
 }
