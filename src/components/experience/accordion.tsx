6c867dde53bbbe0cada454f46a06a5b03fa8c0c5
Szabi Keresztes <szabikr.dev@gmail.com>
2023-04-16 19:53:07 +0300

    Scroll accordion items to the top of screen when expanded (#74)

--- src/components/experience/accordion.tsx
+++ src/components/experience/accordion.tsx
@@ -1,15 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { AccordionProps } from '../../types/props'
 
 export default function Accordion({ items, openByDefault }: AccordionProps) {
   const [show, setShow] = useState(openByDefault)
+  const accordionRef = useRef(null)
+  const didMount = useRef(false)
 
   const toggleItem = (id: string) => (show === id ? setShow(null) : setShow(id))
 
+  useEffect(() => {
+    // Do not run on first render
+    if (!didMount.current) {
+      didMount.current = true
+      return
+    }
+
+    if (show === null) {
+      return
+    }
+
+    const accordionNode = accordionRef.current
+    const itemNode = accordionNode.querySelector(`#accordion-item-${show}`)
+    itemNode.scrollIntoView({ behavior: 'smooth' })
+  }, [show])
+
   return (
-    <div className="accordion">
+    <div className="accordion" ref={accordionRef}>
       {items.map((item) => (
-        <div key={item.id} className="accordion-item">
+        <div
+          key={item.id}
+          id={`accordion-item-${item.id}`}
+          className="accordion-item"
+        >
           <div className="accordion-header">
             {item.header(item.id != show)}
             <div>
